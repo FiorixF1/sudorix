d8022af6cc88803a27ba4b1c7811203a84132e63
Alessandro Fiorillo <alessandro.fiorillo@mail.polimi.it>
2026-07-11 11:37:41 +0200

    Plibonigo en Sue-de-Coq

--- src/solver.cpp
+++ src/solver.cpp
@@ -1458,77 +1458,94 @@ static void techSueDeCoq(SudokuBoard &board, EventQueue &eventQueue) {
   // extended Sue-de-Coq
   auto scanExtendedSDC = [&](const Unit &box, const Unit &line) -> bool
   {
-    CellSet intersection = (box & line).filter([&](Cell i){ return !board.isSolved(i); });
-    DigitSet intersectionDigits;
-    for (Cell idx : intersection) {
-      intersectionDigits |= board.getCandidates(idx);
+    std::vector<CellSet> intersections;
+    CellSet mainIntersection = (box & line).filter([&](Cell i){ return !board.isSolved(i); });
+    intersections.push_back(mainIntersection);
+
+    // add the possibility to consider only 2 cells when the box-line intersection contains 3 cells
+    if (mainIntersection.size() == 3) {
+      auto first  = mainIntersection.begin();
+      auto second = ++first;
+      auto third  = ++second;
+
+      intersections.push_back(CellSet({*first}) | CellSet({*second}));
+      intersections.push_back(CellSet({*first}) | CellSet({*third}));
+      intersections.push_back(CellSet({*second}) | CellSet({*third}));
     }
-    int intersectionSize = intersection.size();
 
-    if (intersectionDigits.size() >= intersectionSize + 2) {
-      // get unsolved locations in box outside intersection
-      std::vector<int> boxList = box.to_vector();
-      LocationSet boxUnsolved;
-      for (Location i = 0; i < 9; ++i) {
-        if (!board.isSolved(boxList[i]) && !intersection.contains(boxList[i])) {
-          boxUnsolved.insert(i);
-        }
+    // then test every possible combination
+    for (CellSet intersection : intersections) {
+      DigitSet intersectionDigits;
+      for (Cell idx : intersection) {
+        intersectionDigits |= board.getCandidates(idx);
       }
+      int intersectionSize = intersection.size();
 
-      // get unsolved locations in line outside intersection
-      std::vector<int> lineList = line.to_vector();
-      LocationSet lineUnsolved;
-      for (Location i = 0; i < 9; ++i) {
-        if (!board.isSolved(lineList[i]) && !intersection.contains(lineList[i])) {
-          lineUnsolved.insert(i);
-        }
-      }
-
-      for (int boxSubsetSize = 1; boxSubsetSize <= boxUnsolved.size(); ++boxSubsetSize) {
-        std::vector<LocationSet> subsets = boxUnsolved.generate_power_set_of_size(boxSubsetSize);
-        for (const LocationSet &boxSubset : subsets) {
-          CellSet boxCells;
-          DigitSet boxDigits;
-
-          for (Location l : boxSubset) {
-            Cell idx = static_cast<Cell>(boxList[l]);
-            DigitSet candidates = board.getCandidates(idx);
-            boxCells.insert(idx);
-            boxDigits |= candidates;
+      if (intersectionDigits.size() >= intersectionSize + 2) {
+        // get unsolved locations in box outside intersection
+        std::vector<int> boxList = box.to_vector();
+        LocationSet boxUnsolved;
+        for (Location i = 0; i < 9; ++i) {
+          if (!board.isSolved(boxList[i]) && !intersection.contains(boxList[i])) {
+            boxUnsolved.insert(i);
           }
+        }
 
-          for (int lineSubsetSize = 1; lineSubsetSize <= lineUnsolved.size(); ++lineSubsetSize) {
-            std::vector<LocationSet> subsets = lineUnsolved.generate_power_set_of_size(lineSubsetSize);
-            for (const LocationSet &lineSubset : subsets) {
-              CellSet lineCells;
-              DigitSet lineDigits;
+        // get unsolved locations in line outside intersection
+        std::vector<int> lineList = line.to_vector();
+        LocationSet lineUnsolved;
+        for (Location i = 0; i < 9; ++i) {
+          if (!board.isSolved(lineList[i]) && !intersection.contains(lineList[i])) {
+            lineUnsolved.insert(i);
+          }
+        }
 
-              for (Location l : lineSubset) {
-                Cell idx = static_cast<Cell>(lineList[l]);
-                DigitSet candidates = board.getCandidates(idx);
-                lineCells.insert(idx);
-                lineDigits |= candidates;
-              }
+        for (int boxSubsetSize = 1; boxSubsetSize <= boxUnsolved.size(); ++boxSubsetSize) {
+          std::vector<LocationSet> subsets = boxUnsolved.generate_power_set_of_size(boxSubsetSize);
+          for (const LocationSet &boxSubset : subsets) {
+            CellSet boxCells;
+            DigitSet boxDigits;
 
-              // Sue-de-Coq test
-              if ((boxDigits & lineDigits).empty()) {
-                if ((intersection | boxCells | lineCells).size() == (intersectionDigits | boxDigits | lineDigits).size()) {
-                  DigitSet allDigits = intersectionDigits | boxDigits | lineDigits;
-                  DigitSet extraDigit = intersectionDigits - boxDigits - lineDigits;
-                  // extended Sue-de-Coq spotted
-                  Event event(EventType::RemoveCandidate, ReasonId::SueDeCoq);
-                  // the source is the cells in intersection, line and box
-                  event.addSource(lineCells | intersection, lineDigits);
-                  event.addSource(boxCells | intersection, boxDigits);
-                  if (!extraDigit.empty()) event.addSource(intersection, extraDigit);
-                  // eliminate along line and inside the box
-                  for (Cell idx : line - intersection - lineCells) {
-                    event.addOperation(idx, lineDigits | extraDigit);
+            for (Location l : boxSubset) {
+              Cell idx = static_cast<Cell>(boxList[l]);
+              DigitSet candidates = board.getCandidates(idx);
+              boxCells.insert(idx);
+              boxDigits |= candidates;
+            }
+
+            for (int lineSubsetSize = 1; lineSubsetSize <= lineUnsolved.size(); ++lineSubsetSize) {
+              std::vector<LocationSet> subsets = lineUnsolved.generate_power_set_of_size(lineSubsetSize);
+              for (const LocationSet &lineSubset : subsets) {
+                CellSet lineCells;
+                DigitSet lineDigits;
+
+                for (Location l : lineSubset) {
+                  Cell idx = static_cast<Cell>(lineList[l]);
+                  DigitSet candidates = board.getCandidates(idx);
+                  lineCells.insert(idx);
+                  lineDigits |= candidates;
+                }
+
+                // Sue-de-Coq test
+                if ((boxDigits & lineDigits).empty()) {
+                  if ((intersection | boxCells | lineCells).size() == (intersectionDigits | boxDigits | lineDigits).size()) {
+                    DigitSet allDigits = intersectionDigits | boxDigits | lineDigits;
+                    DigitSet extraDigit = intersectionDigits - boxDigits - lineDigits;
+                    // extended Sue-de-Coq spotted
+                    Event event(EventType::RemoveCandidate, ReasonId::SueDeCoq);
+                    // the source is the cells in intersection, line and box
+                    event.addSource(lineCells | intersection, lineDigits);
+                    event.addSource(boxCells | intersection, boxDigits);
+                    if (!extraDigit.empty()) event.addSource(intersection, extraDigit);
+                    // eliminate along line and inside the box
+                    for (Cell idx : line - intersection - lineCells) {
+                      event.addOperation(idx, lineDigits | extraDigit);
+                    }
+                    for (Cell idx : box - intersection - boxCells) {
+                      event.addOperation(idx, boxDigits | extraDigit);
+                    }
+                    if (eventQueue.enqueue(board, event)) return true;
                   }
-                  for (Cell idx : box - intersection - boxCells) {
-                    event.addOperation(idx, boxDigits | extraDigit);
-                  }
-                  if (eventQueue.enqueue(board, event)) return true;
                 }
               }
             }
